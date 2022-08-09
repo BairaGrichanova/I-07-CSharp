93baafe5ca4de7a826206ae5418828ec76a31b40
Baya Grichanova <bg.red_line@bk.ru>
2022-08-09 20:25:18 +0300

    I-07 Seminars - add Example 1

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1. Напишите программу, которая выводит случайное 
+//число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int FindBiggerDigit(int number)
+{
+    int ed = number % 10;
+    int des = number / 10;
+    if (ed > des) return(ed);
+    else return(des);
+}
+
+int rand = new Random().Next (10,99);
+
+Console.WriteLine(rand);
+Console.WriteLine (FindBiggerDigit(rand));
