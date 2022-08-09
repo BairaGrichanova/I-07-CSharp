b8b4e2493a0fd28d1009f092e78fd69020e944b2
Baya Grichanova <bg.red_line@bk.ru>
2022-08-09 20:46:11 +0300

    I-07 Seminars - add Examamle 2

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 1. Напишите программу, которая выводит случайное 
 //число из отрезка [10, 99] и показывает наибольшую цифру числа.
-
+/*
 int FindBiggerDigit(int number)
 {
     int ed = number % 10;
@@ -11,7 +11,23 @@ int FindBiggerDigit(int number)
 
 int rand = new Random().Next (10,100);
 
-Console.WriteLine("Случайное двузначное число: "+rand);
+Console.WriteLine("Случайное двухзначное число: "+rand);
 Console.WriteLine ("Наибольшая цифра в числе, это: "+FindBiggerDigit(rand));
+*/
+// Задача 2. Напишите программу, которая выводит случайное трёхзначное 
+//число и удаляет вторую цифру этого числа.
+
+int CutTwoNumber (int number)
+{
+    int ed = number % 10;
+    int sot = number / 100;
+    int result = sot * 10 + ed;
+    return(result);
+}
+
+int rand = new Random().Next (100,1000);
+
+Console.WriteLine("Случайное трёхзначное число: "+rand);
+Console.WriteLine ("Убираем вторую цифру в числе: "+CutTwoNumber(rand));
 
 // Задача 2. 
