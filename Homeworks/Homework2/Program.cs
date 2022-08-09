0749a8297e4ec370c0511fafe7dcc20db030999b
Baya Grichanova <bg.red_line@bk.ru>
2022-08-09 22:29:55 +0300

    I-07 Homeworks - add Task 13 & Task 15

--- Homeworks/Homework2/Program.cs
+++ Homeworks/Homework2/Program.cs
@@ -1,2 +1,51 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+void Task10 (int number)
+{
+    int result = number % 100 / 10;
+    Console.WriteLine (result);
+}
 
+Console.Write ("Введите трёхзначное число: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+
+Task10 (n);
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+void Task13 (int number)
+{
+    int result = 0;
+    int razr = 100;
+    if (number < 100 & number > -100) Console.WriteLine ($"Введённое число: {number}, третьей цифры в числе нет");
+    else 
+    {
+        while (razr < number) 
+        {
+            if (number < razr*10) result = number % (razr/10) / (razr/100);
+            razr = razr * 10;
+        }
+        Console.WriteLine ("Третья цифра в числе: "+result);
+    }
+}
+
+Console.Write ("Введите целое число от -2 147 483 648 до 2 147 483 647: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+
+Task13 (n);
+*/
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+void Task15 (int numberDay)
+{
+    if (numberDay < 6) Console.WriteLine ("Пора на работу");
+    else Console.WriteLine ("Выходной");
+}
+
+Console.Write ("Введите день недели (число от 1 до 7): ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+
+Task15 (n);
+*/
