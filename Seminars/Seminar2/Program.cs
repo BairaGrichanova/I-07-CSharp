c200256b983d3aac0ef53dd5410328397434f3f4
Baya Grichanova <bg.red_line@bk.ru>
2022-08-09 20:57:47 +0300

    I-07 Seminars - add Examamle 3

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -14,9 +14,11 @@ int rand = new Random().Next (10,100);
 Console.WriteLine("Случайное двухзначное число: "+rand);
 Console.WriteLine ("Наибольшая цифра в числе, это: "+FindBiggerDigit(rand));
 */
+
+
 // Задача 2. Напишите программу, которая выводит случайное трёхзначное 
 //число и удаляет вторую цифру этого числа.
-
+/*
 int CutTwoNumber (int number)
 {
     int ed = number % 10;
@@ -29,5 +31,23 @@ int rand = new Random().Next (100,1000);
 
 Console.WriteLine("Случайное трёхзначное число: "+rand);
 Console.WriteLine ("Убираем вторую цифру в числе: "+CutTwoNumber(rand));
+*/
 
-// Задача 2. 
+
+// Задача 3. Напишите программу, которая будет принимать на вход 
+//два числа и выводить, является ли второе число кратным первому. 
+//Если второе число не кратно числу первому, то программа выводит 
+//остаток от деления.
+
+void Example1 (int num1, int num2)
+{
+    int ost = num2 % num1;
+    if (ost == 0) Console.WriteLine ($"{num2} кратно {num1}");
+    else Console.WriteLine ($"{num2} не кратно {num1}. Остаток деления: {ost}");
+}
+
+Console.Write ("Введите первое число: ");
+int n1 = Convert.ToInt32 (Console.ReadLine ());
+Console.Write ("Введите второе число: ");
+int n2 = Convert.ToInt32 (Console.ReadLine ());
+Example1 (n1,n2);
