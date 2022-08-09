5632e3151753a3c50a4fe7af43a436d82fa071df
Baya Grichanova <bg.red_line@bk.ru>
2022-08-09 16:45:17 +0300

    I-07 Seminars - Create New Project

---
+++ Homeworks/Homework1/Task6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write ("Введите целое число: ");
+int num = Convert.ToInt32 (Console.ReadLine ());
+int even = num%2;
+if (even==0) Console.Write ("Вы ввели чётное число");
+else         Console.Write ("Вы ввели нечётное число");
