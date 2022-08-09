e13664e9f76cd09483679169eb48900faee6a7e8
Baya Grichanova <bg.red_line@bk.ru>
2022-08-09 21:07:42 +0300

    I-07 Seminars - add Examamle 4

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -38,16 +38,31 @@ Console.WriteLine ("Убираем вторую цифру в числе: "+CutT
 //два числа и выводить, является ли второе число кратным первому. 
 //Если второе число не кратно числу первому, то программа выводит 
 //остаток от деления.
-
-void Example1 (int num1, int num2)
+/*
+void Example3 (int num1, int num2)
 {
     int ost = num2 % num1;
     if (ost == 0) Console.WriteLine ($"{num2} кратно {num1}");
     else Console.WriteLine ($"{num2} не кратно {num1}. Остаток деления: {ost}");
 }
-
 Console.Write ("Введите первое число: ");
 int n1 = Convert.ToInt32 (Console.ReadLine ());
 Console.Write ("Введите второе число: ");
 int n2 = Convert.ToInt32 (Console.ReadLine ());
-Example1 (n1,n2);
+Example3 (n1,n2);
+*/
+
+// Задача 4. Напишите программу, которая принимает на вход число 
+//и проверяет, кратно ли оно одновременно 7 и 23.
+
+bool Example4 (int number)
+{
+//    if (number%7==0 && number%23==0) return true;
+//    else return false;
+    return number%7==0 && number%23==0;
+}
+
+Console.Write ("Введите целое число: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+bool result = Example4(n);
+Console.WriteLine(result);
