b621dce989347dc02a666e90d534a5a891693a24
Baya Grichanova <bg.red_line@bk.ru>
2022-08-16 20:36:36 +0300

    I-07 Seminars 4 - add task 1 & task 2

--- Seminars/Seminar4/Program.cs
+++ Seminars/Seminar4/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Найти сумму чисел от 1 до А
-
+/*
 int FindSum (int number)
 {
     int sum = 0;
@@ -17,4 +17,51 @@ Console.Write ("Введите целое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int result = FindSum(a);
 Console.WriteLine ($"Сумма чисел от 1 до {a} будет {result}");
+*/
+
+// Задача 1. Напишите программу, которая принимает на вход число и 
+// выдаёт количество цифр в числе.
+/*
+int Number (int n)
+{
+    int count = 1;
+    if (n<0) n*=(-1);
+    while (n>10)
+    {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write ("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ($"В числе: {a} {Number(a)} цифр");
+*/
+
+// Задача 2. Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+/*
+int FindSum (int number)
+{
+    int sum = 1;
+
+    for (int current = 1; current <= number; current++)
+    {
+        //sum = sum * current;
+        sum *= current;
+    }
+
+    return sum;
+}
+
+Console.Write ("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = FindSum(a);
+Console.WriteLine ($"Произведение чисел от 1 до {a} будет {result}");
+*/
+
+// Задача 3. Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+
 
