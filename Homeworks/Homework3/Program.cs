553d93c89dd9cf91c341f4b5604799907cb1aad6
Baya Grichanova <bg.red_line@bk.ru>
2022-08-16 17:55:53 +0300

    I-07 Seminars 3 - Homework 3 - create Task 19

--- Homeworks/Homework3/Program.cs
+++ Homeworks/Homework3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+bool Task19(int number)
+{
+    bool rezult = false;
+
+    int n1 = number % 10;
+    int n2 = number / 10000;
+    number = number % 10000 / 10;
+
+    if (n1 == n2)
+    {
+        n1 = number % 10;
+        n2 = number / 100;
+        if (n1 == n2) rezult = true;
+    }
+
+    return rezult;
+}
+
+Console.Write("Введите целое пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (Task19(n)) Console.Write("Полиндром");
+else Console.Write("Не полиндром");
