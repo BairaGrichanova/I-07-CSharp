bd6017c1ed4040380325b86af616fc67e5c4dee5
Baya Grichanova <bg.red_line@bk.ru>
2022-08-16 20:11:38 +0300

    I-07 Seminars 4 - add first example

--- Seminars/Seminar4/Program.cs
+++ Seminars/Seminar4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму чисел от 1 до А
+
+int FindSum (int number)
+{
+    int sum = 0;
+
+    for (int current = 1; current <= number; current++)
+    {
+        //sum = sum + current;
+        sum += current;
+    }
+
+    return sum;
+}
+
+Console.Write ("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = FindSum(a);
+Console.WriteLine ($"Сумма чисел от 1 до {a} будет {result}");
+
