9c7f38aee4702bb61662b28d8c2c137d5970e3e5
Baya Grichanova <bg.red_line@bk.ru>
2022-08-17 12:39:52 +0300

    I-07 Homeworks - Homework 4 - add Task 29

--- Homeworks/Homework4/Program.cs
+++ Homeworks/Homework4/Program.cs
@@ -4,11 +4,11 @@
 long Stepen (int n, int x)
 {
     long s=1;
-    for (int i=1; i<=x; i++) s = s*n;
+    for (int i=1; i<=x; i++) s *= n;
     return s;
 }
 
-Console.WriteLine ("Программа возведения числа в степень.");
+Console.WriteLine ("Программа возведения числа в степень. Для выхода из программы нажмите Ctrl+C");
 Console.Write ("Введите число (А): ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write ("Введите степень (В): ");
@@ -38,3 +38,32 @@ int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ($"Сумма цифр в числе {number} будет равна {SumNumber (number)}");
 */
 
+// Task 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// * Свой вариан метода CreateArray, произвольный размер массива
+
+int[] CreateArray (int size)
+{
+    int[] newArray = new int[size];
+
+    for (int i=0; i<size; i++) 
+    {
+        Console.Write ($"ARRAY[{i}]=");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return newArray;
+}
+void ShowArray (int[] array)
+{
+Console.Write ("Наш массив: ");
+    for (int i = 0; i < array.Length; i++)
+    Console.Write (array[i] + " ");
+    
+Console.WriteLine();
+}
+
+Console.Write ("Введите количество элементов в массиве ARRAY[]: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateArray (a);
+
+ShowArray(myArray);
