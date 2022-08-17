ea65894a4a873967699d3192170081111b79b8ee
Baya Grichanova <bg.red_line@bk.ru>
2022-08-17 10:01:51 +0300

    I-07 Homeworks - Homework 4 - add Task 25

--- Homeworks/Homework4/Program.cs
+++ Homeworks/Homework4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 25. Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+long Stepen (int n, int x)
+{
+    long s=1;
+    for (int i=1; i<=x; i++) s = s*n;
+    return s;
+}
+
+Console.WriteLine ("Программа возведения числа в степень.");
+Console.Write ("Введите число (А): ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите степень (В): ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ($"Число {a} в степени {b} будет равно {Stepen (a,b)}");
