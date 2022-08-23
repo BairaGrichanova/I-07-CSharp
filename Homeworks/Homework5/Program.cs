cabe9b3bb16ed9eaa458bd17e4849f13d9894b58
Baya Grichanova <bg.red_line@bk.ru>
2022-08-23 13:52:13 +0300

    Seminars - Homework 5 - add Task 36

--- Homeworks/Homework5/Program.cs
+++ Homeworks/Homework5/Program.cs
@@ -38,4 +38,46 @@ ShowArray(myArray);
 Console.WriteLine ($"Количество чётных чисел в массиве {FindEvenNumber(myArray)}");
 */
 
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+/*
+int[] CreateRandomArray (int size, int minVal, int maxVal)
+{
+    int[] newArray = new int[size];
+
+    for (int i=0; i<size; i++) newArray[i] = new Random().Next(minVal,maxVal + 1);
+    return newArray;
+}
+void ShowArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write (array[i] + " ");
+    
+Console.WriteLine();
+}
+int SumEvenNumber(int[] array)
+{
+    int k=0;
+    for (int i = 1; i < array.Length; i+=2)
+        {
+            k+=array[i];
+        }
+    return k;
+}
+
+Console.WriteLine ("Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
+Console.Write ("Введите количество элементов массива ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите минимальное значение диапазона (от 100) ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите максимальное значение диапазона (до 999) ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray (a, min, max);
+ShowArray(myArray);
+Console.WriteLine ($"Сумма чисел на нечётных позициях: {SumEvenNumber(myArray)}");
+*/
+
+
 
