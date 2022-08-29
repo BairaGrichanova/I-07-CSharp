858e5daf50a98ad036774fe8db9441a032d87b3d
Baya Grichanova <bg.red_line@bk.ru>
2022-08-29 12:50:45 +0300

    Seminars - homework 7 - add folder for homework, add task 50

--- Homeworks/Homework7/Program.cs
+++ Homeworks/Homework7/Program.cs
@@ -16,7 +16,6 @@ double[,] CreateRandomDoubleArray(int rows, int columns, int minValue, int maxVa
         }
     return newArray;
 }
-
 void ShowDoubleArray(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -39,3 +38,49 @@ double[,] myArray = CreateRandomDoubleArray(m, n, min, max);
 ShowDoubleArray(myArray);
 */
 
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+/*
+int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int k = 0; k < columns; k++)
+            newArray[i, k] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+            Console.Write(array[i, k] + " ");
+        Console.WriteLine();
+    }
+}
+void SearchByCoordinates (int[,] array, int i, int k)
+{
+    if (i<array.GetLength(0) && k<array.GetLength(1))
+        Console.Write("На указанной позиции располагается число: "+array[i, k]);
+    else
+        Console.Write("Такой позиции в нашей матрице не имеется");
+}
+
+int m = 10,
+    n = 10,
+    min = -10,
+    max = 10;
+int[,] myArray = CreateRandom2DArray(m, n, min, max);
+Show2DArray(myArray);
+
+Console.Write("Введите № строки для поиска элемента: ");
+int f1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца для поиска элемента: ");
+int f2 = Convert.ToInt32(Console.ReadLine());
+
+SearchByCoordinates(myArray,f1,f2);
+*/
