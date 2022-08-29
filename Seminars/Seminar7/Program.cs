05b6c4f925b0afad7dfec61d93f8dd28894b9239
Baya Grichanova <bg.red_line@bk.ru>
2022-08-29 11:17:44 +0300

    Seminars - seminar 7 - add task 1

--- Seminars/Seminar7/Program.cs
+++ Seminars/Seminar7/Program.cs
@@ -1,5 +1,5 @@
 ﻿// first Task. Заполнить двумерный массив случайными целыми числами
-
+/*
 int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] newArray = new int[rows, columns];
@@ -29,3 +29,39 @@ Console.Write("Введите максимальное значение: ");
 int max = Convert.ToInt32(Console.ReadLine());
 int[,] myArray = CreateRandom2DArray(m, n, min, max);
 Show2DArray(myArray);
+*/
+
+// Задать двумерный массив по формуле
+//a[i,j]=i+j
+/*
+int[,] Create2DArray(int rows, int columns)
+{
+    int[,] newArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int k = 0; k < columns; k++)
+            newArray[i, k] = i+k;
+    return newArray;
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+            Console.Write(array[i, k] + " ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = Create2DArray(m, n);
+Show2DArray(myArray);
+*/
+
+// Задать двумерный массив, найти элементы, у которых оба индекса чётные, и возвести эти элементы в квадрат
+
+// Необходимо задать двумерный массив и найти сумму элементов на главной диагонали (индексы совпадают)
